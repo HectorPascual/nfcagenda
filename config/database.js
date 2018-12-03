1137be24f643f8482a7be09c9aab011145cc0532
Unknown <rcpascualhector@gmail.com>
2018-12-03 18:55:46 +0100

    changed db

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
   // configure the code below with your username, password and mlab database information
-  database: 'mongodb://localhost:27017/mydb',    //dev
+  database: 'mongodb://hector:admin1@ds147181.mlab.com:47181/pbe',    //dev
   secret: 'yoursecret'
 }
