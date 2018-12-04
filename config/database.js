771c2448e2b3122503ca9672842187ec7ac2829c
Unknown <rcpascualhector@gmail.com>
2018-12-04 17:06:10 +0100

    fixed login

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   // configure the code below with your username, password and mlab database information
   //database: 'mongodb://<username>:<password>@ds12226.mlab.com:12226/meanauthapp',   //prod
-  database: 'mongodb://user:admin1d@ds245680.mlab.com:45680/pbe',    //dev
+  database: 'mongodb://user:admin1@ds245680.mlab.com:45680/pbe',    //dev
   secret: 'yoursecret'
 }
